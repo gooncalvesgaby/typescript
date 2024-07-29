851db757e2b5e2146b3bfb5ae32ed79c70248fb5
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-07-29 14:36:05 -0300

    estudos sobre type annotations

---
+++ src/aula01-typeannotations/typeannotations.ts
@@ -0,0 +1,30 @@
+// em ts type annotations é uma maneira de declarar explicitamente o 
+//tipo de uma variável, função ou paramêtro de função.
+
+let nome: string = "gaby";
+let idade: number = 29;
+let adulta: boolean = true;
+
+// criando um Array de Strings
+
+let arrayString: Array<string> = ["Gabrielle", "Gonçalves", "Rocha"];
+let arrayString2: string[] = ["Otavio", "Capel", "Garcia"]
+
+// criando um Array de Numbers
+
+let birthdayData: Array<number> = [5, 5, 1995];
+let birthdayData2: number[] = [18, 5, 1995];
+
+// OBJECT
+
+let pessoa: {nome: string, idade: number, adulto: boolean} = {
+    nome: "Gabrielle",
+    idade: 29,
+    adulto: true
+
+};
+
+
+
+
+
