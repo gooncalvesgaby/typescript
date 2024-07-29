ecb99b251ff2dcaf822f0d4040d263631e7bc8c4
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-07-29 15:10:45 -0300

    type any

---
+++ src/aula02-typeany/typeany.ts
@@ -0,0 +1,8 @@
+function message(msg: any) {
+    return msg;
+}
+
+console.log(message("olá eu sou a Gaby"));
+console.log(message([5, 5, 95]));
+console.log(message(29));
+console.log(message(true));
